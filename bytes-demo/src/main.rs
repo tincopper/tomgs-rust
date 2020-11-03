c89e7d96580fbf406435bb149dd868a4347e2706
tomgs <136082619@qq.com>
2020-11-03 15:52:34 +0800

    add 1103

---
+++ bytes-demo/src/main.rs
@@ -0,0 +1,21 @@
+use bytes::{Bytes, BytesMut, Buf, BufMut};
+
+// bytes crate demo
+// bytes: A cheaply cloneable and sliceable chunk of contiguous memory.
+fn main() {
+  let mut buf = BytesMut::with_capacity(1024);
+
+  // put需要传入一个Buf，其表示方法如下let mut buf: Buf = &b"hello world"[..];
+  buf.put(&b"hello word"[..]);
+  buf.put_u16(1234);
+
+  let a = buf.split();
+  //assert_eq!(a, b"hello world\x04\xD2"[..]);
+
+  buf.put(&b"goodbye world"[..]);
+
+  let b = buf.split();
+  assert_eq!(b, b"goodbye world"[..]);
+
+  assert_eq!(buf.capacity(), 999);
+}
--- db_demo/src/pg_demo.rs
+++ db_demo/src/pg_demo.rs
@@ -10,7 +10,7 @@ struct Person {
 /// 测试pg数据库操作
 /// https://docs.rs/postgres/0.15.2/postgres/
 pub fn pg_exer() {
-  let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#2019 dbname=private_cloud_dev", NoTls).unwrap();
+  let mut client = Client::connect("host=localhost user=root port=5432 password=123456 dbname=test", NoTls).unwrap();
 
   // 参数化查询
   let result = &client.query("SELECT id, name, data FROM person", &[]);
@@ -32,8 +32,8 @@ pub fn pg_exer() {
 }
 
 pub fn pg_simple_query(sql: &str) {
-  //let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#2019 dbname=private_cloud_dev", NoTls).unwrap();
-  let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#830c73f3fb22 dbname=jdyapp_shard3", NoTls).unwrap();
+  //let mut client = Client::connect("host=localhost user=root port=5432 password=123456 dbname=test", NoTls).unwrap();
+  let mut client = Client::connect("host=localhost user=root port=5432 password=123456 dbname=test", NoTls).unwrap();
 
   // 简单查询
   let result = &client.simple_query(sql);
@@ -62,9 +62,7 @@ pub fn pg_simple_query(sql: &str) {
 
 //  172.20.183.155
 pub fn pg_parse_query(sql: &str, params: &[&(dyn ToSql + Sync)]) {
-  //let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#2019 dbname=private_cloud_dev", NoTls).unwrap();
-  let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#830c73f3fb22 dbname=jdyapp_shard3", NoTls).unwrap();
-  //let mut client = Client::connect("postgresql://127.0.0.1:5432/private_cloud_dev?autoReconnect=true&useSSL=false&useUnicode=true&characterEncoding=utf8&currentSchema=kd_1596523828369351661", NoTls).unwrap();
+  let mut client = Client::connect("host=localhost user=root port=5432 password=123456 dbname=test", NoTls).unwrap();
   // 简单查询
   let result = client.query(sql, params);
   match result {
@@ -82,7 +80,7 @@ pub fn pg_parse_query(sql: &str, params: &[&(dyn ToSql + Sync)]) {
 }
 
 pub fn pg_execute(sql: &str, params: &[&(dyn ToSql + Sync)]) {
-  let mut client = Client::connect("host=localhost user=jdy port=5432 password=Jdy#2019 dbname=private_cloud_dev", NoTls).unwrap();
+  let mut client = Client::connect("host=localhost user=root port=5432 password=123456 dbname=test", NoTls).unwrap();
   let result = client.execute(sql, params);
 
   println!("result: {:?}", result.unwrap());
@@ -104,28 +102,6 @@ pub fn test2() {
 pub fn test3() {
   let sql = "select count(*) from t_account_balance;";
   pg_simple_query(sql);
-}
-
-#[test]
-pub fn test4() {
-  // /*schema|where_condition|join_condition|order_by_condition|group_by_condtion|having_codition*/
-  //let sql = "/*public|id='7aea0712-9f13-4c50-9eb7-906cf4ea55da'||id||*/select * from users t;";
-  let sql = "/*kd_1600849898390490952|accountid='123abc-ddd'|accountid,accountname|||*/
-    select su.fid, su.fnumber, sul.fcomment from t_sec_user as su
-    left join t_sec_user_l as sul on su.fid = sul.fid
-    left join t_other_table as tt on su.id = tt.id;";
-  pg_simple_query(sql);
-}
-
-#[test]
-pub fn test41() {
-  // /*schema|where_condition|join_condition|order_by_condition|group_by_condtion|having_codition*/
-  //let sql = "/*public|id='7aea0712-9f13-4c50-9eb7-906cf4ea55da'||id||*/select * from users t;";
-  let sql = "/*kd_1600849898390490952|accountid='123abc-ddd'|accountid,accountname|||*/
-    select su.fid, su.fnumber, sul.fcomment from t_sec_user as su
-    left join t_sec_user_l as sul on su.fid = sul.fid
-    left join t_other_table as tt on su.id = tt.id;";
-  pg_parse_query(sql, &[]);
 }
 
 #[test]
@@ -154,84 +130,6 @@ pub fn test7() {
   let sql = "/*public|id='7aea0712-9f13-4c50-9eb7-906cf4ea55da'||||*/select * from public.users t where name = $1";
   let name = "tomgs";
   pg_parse_query(sql, &[&name]);
-}
-
-#[test]
-pub fn test8() {
-  let sql = "SELECT
-  t1.fnumber,
-  t1.fphone,
-  t1.feid,
-  t1.fuid,
-  t1.fid,
-  t2.ftruename ftruename,
-  t1.fopenid,
-  fusertype userType,
-  t3.fisactived,
-  t3.fisregisted,
-  CASE WHEN pos.fdptid IS NULL THEN
-    0
-  ELSE
-    pos.fdptid
-  END AS fdptid,
-  udo.forgid forgid
-FROM
-  kd_1596523828369351661.t_sec_user t1
-  INNER JOIN kd_1596523828369351661.t_sec_user_l t2 ON t1.fid = t2.fid
-  LEFT JOIN kd_1596523828369351661.t_sec_userposition pos ON t1.fid = pos.fid
-  LEFT JOIN kd_1596523828369351661.t_bas_userdefaultorg udo ON t1.fid = udo.fuserid
-  LEFT JOIN kd_1596523828369351661.t_sec_user_u t3 ON t1.fid = t3.fid
-WHERE
-  t1.fnumber = $1;";
-
-  let number = "kingdeetestzcy";
-  pg_parse_query(sql, &[&number]);
-}
-
-#[test]
-pub fn test81() {
-  let sql = "SELECT
-  t1.fnumber,
-  t1.fphone,
-  t1.feid,
-  t1.fuid,
-  t1.fid,
-  t2.ftruename ftruename,
-  t1.fopenid,
-  fusertype userType,
-  t3.fisactived,
-  t3.fisregisted,
-  CASE WHEN pos.fdptid IS NULL THEN
-    0
-  ELSE
-    pos.fdptid
-  END AS fdptid,
-  udo.forgid forgid
-FROM
-  t_sec_user t1
-  INNER JOIN t_sec_user_l t2 ON t1.fid = t2.fid
-  LEFT JOIN t_sec_userposition pos ON t1.fid = pos.fid
-  LEFT JOIN t_bas_userdefaultorg udo ON t1.fid = udo.fuserid
-  LEFT JOIN t_sec_user_u t3 ON t1.fid = t3.fid
-WHERE
-  t1.fnumber = $1;";
-
-  let number = "kingdeetestzcy";
-  pg_parse_query(sql, &[&number]);
-}
-
-#[test]
-pub fn test82() {
-  let sql = "SELECT A.FId AS \"materialid\", A.fisinvauxset AS \"isinvauxset\", A.finvmgrtype AS \"inv_mgr_type\", A.fmininventoryqty AS \"mininventoryqty\", A.fmaxinventoryqty AS \"maxinventoryqty\"
-, A.fsecinventoryqty AS \"secinventoryqty\", B.fspaceid AS \"store_entity.space_spid\", B.fstoreid AS \"store_entity.store_storeid\", B.fauxid AS \"store_entity.store_auxid\", B.fminqty AS \"store_entity.store_minqty\"
-	, B.fmaxqty AS \"store_entity.stroe_maxqty\", B.fsecqty AS \"store_entity.stroe_secqty\"
-FROM \"kd_1602307795488179787\".\"t_bd_material\" a
-	LEFT JOIN \"kd_1602307795488179787\".\"t_bd_materialinvmgrinfo\" b ON B.FId = A.FId
-WHERE A.fenable = $1;";
-
-  let fenable = "1";
-  pg_parse_query(sql, &[&fenable]);
-  //pg_simple_query(sql);
 }
 
 #[test]
