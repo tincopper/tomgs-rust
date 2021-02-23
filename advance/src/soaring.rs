23b6ba0e8e74e63576d1bf4373c385c4d58d2617
tincopper <136082619@qq.com>
2021-02-24 00:14:11 +0800

    add demo

---
+++ advance/src/soaring.rs
@@ -0,0 +1,18 @@
+use clap::App;
+
+fn print_result() {
+    let arg_matches = App::new("img2ascii")
+        .version(0.0.1)
+        .get_matches();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+
+    }
+
+}
